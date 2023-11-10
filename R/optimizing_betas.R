1f67294880aa632d8097618d225bbaaa5600ac13
Charles Benfer <czb0146@auburn.edu>
2023-11-10 09:41:31 -0600

    Some more updated for the rds and NAMESPACE

--- R/optimizing_betas.R
+++ R/optimizing_betas.R
@@ -1,4 +1,5 @@
 #' @title Finding the Betas
+#'
 #' @description This function will spit out the desired \code{beta} values for the
 #' least squares function
 #' @param Beta A \code{vector} containing the desired starting coeffs for estimation
@@ -11,13 +12,13 @@
 #' @importFrom stats
 #' @export
 #' @examples
-#' x <- matrix(rnorm(600), nrow=200)
-#' epsilon <- rnorm(200, 0, sd=.25)
-#' x <- cbind(rep(1,200), x)
-#' actual_beta <- rnorm(4,5,10)
-#' y <- x%*%actual_beta + epsilon
-#' start_beta <- rnorm(4)
-#' betas(Beta=start_beta,y=y, x=x, Norm="L2")
+#'   x <- matrix(rnorm(600), nrow=200)
+#'   epsilon <- rnorm(200, 0, sd=.25)
+#'   x <- cbind(rep(1,200), x)
+#'   actual_beta <- rnorm(4,5,10)
+#'   y <- x%*%actual_beta + epsilon
+#'   start_beta <- rnorm(4)
+#'   betas(Beta=start_beta,y=y, x=x, Norm="L2")
 betas <- function(Beta, f, x, y, Norm){
   optim(Beta, f, Y=y, X=x, norm=Norm)
 }
